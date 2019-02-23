e382b814e4a6fb78af30222d00ca06b79fc53295
DELL <DELL@192.168.56.1>
2019-02-23 23:50:42 +0530

    first commit

---
+++ firstgithubproject/src/main/java/com/FirstProjectDemo/FirstgithubprojectApplication.java
@@ -0,0 +1,13 @@
+package com.FirstProjectDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstgithubprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstgithubprojectApplication.class, args);
+	}
+
+}
